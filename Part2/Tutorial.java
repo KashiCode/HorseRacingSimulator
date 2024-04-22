ba54ecdfd1a7f506c2658017f2e6de1041a29aab
Maks <ostrynskimaks@gmail.com>
2024-04-22 14:55:38 +0100

    Updated Tutorial.java GUI interface with Jlabel.

--- Part2/Tutorial.java
+++ Part2/Tutorial.java
@@ -6,12 +6,15 @@ import java.awt.*;
 public class Tutorial extends JFrame {
     public Tutorial() {
         setTitle("Tutorial");
-        setSize(400, 400);
+        setSize(500, 400);
         setLocationRelativeTo(null);
         setLayout(new BorderLayout());
+
+        JLabel welcomeLabel = new JLabel("Welcome to the the Tutorial", JLabel.CENTER);
+        welcomeLabel.setFont(new Font("Arial", Font.BOLD, 24));
+        add(welcomeLabel, BorderLayout.NORTH);
         
         JTextArea tutorialText = new JTextArea();
-
 
         tutorialText.setText("Step 1: Do this\nStep 2: Do that\n...");
         tutorialText.setEditable(false);
